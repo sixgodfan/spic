79194ff32a289d32701d8227e3f23fbe53db8088
sixgodsix <98725727+sixgodfan@users.noreply.github.com>
2022-10-12 17:18:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-})
+// addEventListener('fetch', event => {
+//     event.respondWith(handleRequest(event.request))
+// })
 
-async function handleRequest(request) {
+// async function handleRequest(request) {
 
-	var background_urls = [
-'https://ww2.sinaimg.cn/large/be6ad69bgw1eys2ubi8loj218g0p0h3y.jpg',
-'https://ww4.sinaimg.cn/large/be6ad69bgw1eys2ubqm6ij218g0p0ql4.jpg',
-'https://ww3.sinaimg.cn/large/be6ad69bgw1eys2ubroxej218g0p0nki.jpg',
-'https://ww3.sinaimg.cn/large/be6ad69bgw1eys2ubpohkj218g0p01a1.jpg',
-'https://ww2.sinaimg.cn/large/be6ad69bgw1eys2ubkx6ij218g0p07ht.jpg'
- /*需要随机到的图片的链接*/
-    ]
-	var index = Math.floor((Math.random()*background_urls.length));
-	res = await fetch(background_urls[index])
-    return new Response(res.body, {
-        headers: { 'content-type': 'image/jpeg' },
-    })
-}
+// 	var background_urls = [
+// 'https://ww2.sinaimg.cn/large/be6ad69bgw1eys2ubi8loj218g0p0h3y.jpg',
+// 'https://ww4.sinaimg.cn/large/be6ad69bgw1eys2ubqm6ij218g0p0ql4.jpg',
+// 'https://ww3.sinaimg.cn/large/be6ad69bgw1eys2ubroxej218g0p0nki.jpg',
+// 'https://ww3.sinaimg.cn/large/be6ad69bgw1eys2ubpohkj218g0p01a1.jpg',
+// 'https://ww2.sinaimg.cn/large/be6ad69bgw1eys2ubkx6ij218g0p07ht.jpg'
+//  /*需要随机到的图片的链接*/
+//     ]
+// 	var index = Math.floor((Math.random()*background_urls.length));
+// 	res = await fetch(background_urls[index])
+//     return new Response(res.body, {
+//         headers: { 'content-type': 'image/jpeg' },
+//     })
+// }
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript asdad 撒打算</h1>`;
